7a3c9d8c3fd6a8def644526cf4de6937d44558f3
Thien Q. Tran <thientquang@gmail.com>
2020-05-26 20:01:33 +0900

    Code of the first experiment

---
+++ exp_1.py
@@ -0,0 +1,196 @@
+""" Synthesis experiment """
+import sys
+import collections
+
+import numpy as np
+from tqdm import tqdm
+from func.data_generator import norm_data_generator
+from func.tester import ttest_p_calc
+from func.func import open_file, write_list
+
+
+# ===== THE PROPOSED METHOD ======
+def spur(hypotheses, p_values, alpha=0.05):
+    """
+    A simplized procedure of SPUR when all hypotheses are testable
+    -- Input:
+    hypotheses: Indicator array for true(0)/false(1) hypotheses
+    p_values: p_values
+    alpha: The significance level
+    """
+    n = len(hypotheses)
+    p_values = np.copy(p_values)
+
+    # Initialization
+    bucket = alpha  # current significant butget
+    ignored = np.zeros(p_values.shape)  # indicators ignored hypotheses
+    rejected = []  # rejected hypotheses
+    last_p = 0  # p_min of the last iteration
+
+    # SPUR
+    while 1:
+        # step 5: Calculate the threshold
+        tau_t = n - np.sum(ignored)  # See proof of Proposition 5.1
+        threshold = bucket / tau_t + last_p
+
+        # step 6 and 7: Get the index and the p-value of
+        # the most significant hypothesis
+        min_p_index = np.argmin(p_values)
+        min_p = p_values[min_p_index]
+
+        # step 8: Reject rule
+        if min_p <= threshold:
+            # step 9: update bucket
+            bucket = bucket - tau_t * (min_p - last_p) + min_p
+            last_p = min_p
+            rejected.append(min_p_index)  # step 11: Reject hypothesis
+            # step 12: Masking for ignored hypotheses
+            ignored[: min_p_index + 1] = 1
+            p_values[: min_p_index + 1] = np.infty  # also mask the p-values
+        else:
+            break
+
+        # If all hypotheses got ignored or rejected
+        if np.sum(ignored) == n:
+            break
+
+    return np.sort(np.array(rejected))
+
+
+# ===== BONFERRONI =====
+def bonferroni(hypotheses, p_values, alpha=0.05):
+    """
+    The Bonferroni procedure
+    -- Input:
+    hypotheses: Indicator array for true(0)/false(1) hypotheses
+    p_values: p_values
+    alpha: The significance level
+    """
+    n = len(hypotheses)
+    p_values = np.copy(p_values)
+
+    threshold = alpha / n
+    rejected = np.where(p_values <= threshold)[0]
+
+    return rejected
+
+
+# ===== WEIGHTED BONFFERONI =====
+def weighted_bonferroni(hypotheses, p_values, weight="linear", alpha=0.05):
+    """
+    The Bonferroni procedure
+    -- Input:
+    hypotheses: Indicator array for true(0)/false(1) hypotheses
+    p_values: p_values
+    alpha: The significance level
+    """
+    n = len(hypotheses)
+    p_values = np.copy(p_values)
+
+    if weight == "linear":
+        w = (np.arange(n) + 1).astype(float)
+
+    w /= np.sum(w)
+    threshold = alpha * w
+    rejected = np.where(p_values <= threshold)[0]
+
+    return rejected
+
+
+# ===== INVALID SPUR =====
+def invalid_spur(hypotheses, p_values, alpha=0.05):
+    """
+    Invalid version of SPUR where budget are not properly managed   -- Input:
+    hypotheses: Indicator array for true(0)/false(1) hypotheses
+    p_values: p_values
+    alpha: The significance level
+    """
+    n = len(hypotheses)
+    p_values = np.copy(p_values)
+
+    # Initialization
+    ignored = np.zeros(p_values.shape)
+    rejected = []
+
+    # SPUR
+    while 1:
+        # Get the "original index" and the p-value of
+        # the most significant hypothesis
+        min_p_index = np.argmin(p_values)
+        min_p = p_values[min_p_index]
+
+        # Calculate the threshold
+        tau_t = n - np.sum(ignored)
+        threshold = alpha / tau_t
+
+        # Reject rule
+        if min_p <= threshold:
+            rejected.append(min_p_index)
+            ignored[: min_p_index + 1] = 1  # Remove uninteresting hypotheses
+            p_values[: min_p_index + 1] = np.infty  # Also mask the p-values
+        else:
+            break
+
+        # If all hypotheses got ignored or rejected
+        if np.sum(ignored) == n:
+            break
+
+    return np.sort(np.array(rejected))
+
+
+# ===== Experiment setting =====
+# exps: List of settings that will be used in the experiment
+# where each element contains
+# a) the name of the setting (e.g., High usefulness)
+# b) the indicators for false null hypotheses
+# (e.g., '1' means false null hypotheses)
+exps = collections.OrderedDict(
+    [
+        ("High utility", np.array([0] * 80 + [1] * 20)),
+        ("Normal utility", np.array([1, 0, 0, 0, 0] * 20)),
+        ("Low utility", np.array([1] * 20 + [0] * 80)),
+    ]
+)
+
+# methods: List of methods that will be used for each setting
+methods = collections.OrderedDict(
+    [
+        ("SPUR", spur),
+        ("Bonferroni", bonferroni),
+        ("Weighted-Bonferroni", weighted_bonferroni),
+        ("Invalid SPUR", invalid_spur),
+    ]
+)
+
+# ===== main ======
+sub_dir = "exp_1/"
+n_rep = 10000
+np.random.seed(100)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        n_rep = int(sys.argv[1])
+
+    for exp_name, exp_hypotheses in exps.items():
+        # Create file for output based on name of exp and method
+        output_files = {
+            method_name: open_file(sub_dir, exp_name, method_name)
+            for method_name in methods.keys()
+        }
+
+        # Running experiment
+        print(f"Running [{exp_name}]")
+        for f in output_files.values():
+            write_list(f, np.where(exp_hypotheses == 1)[0])
+
+        for i in tqdm(range(n_rep)):
+            datas = norm_data_generator(exp_hypotheses)
+            p_values = ttest_p_calc(datas, mu=0)
+
+            for method_name, method_func in methods.items():
+                rejected = method_func(exp_hypotheses, p_values)
+                write_list(output_files[method_name], rejected)
+
+        # Close file
+        for f in output_files.values():
+            f.close()
---
+++ exp_1_summarizer.py
@@ -0,0 +1,168 @@
+import numpy as np
+import pandas as pd
+
+from matplotlib import pyplot as plt
+from exp_1 import methods, exps, sub_dir
+from func.func import open_file, str_to_array
+
+
+def read_result(f):
+    """
+    Read the result from files f
+    Return the indexes of false and reject_list hypotheses
+    """
+    lines = f.readlines()
+    # Indexes of false null hypotheses
+    false_idxs = str_to_array(lines[0])
+    # Indexes of reject_list hypotheses
+    # Each line contains (possibly multiple) rejections for each run
+    reject_list = [str_to_array(line) for line in lines[1:]]
+    return false_idxs, reject_list
+
+
+def type1_occured(false_idxs, reject_idxs):
+    """
+    Check if there is an type-I error occured
+    i.e., if exists a rejection which is not a false null hypothesis
+    """
+    return int(len(np.union1d(false_idxs, reject_idxs)) > len(false_idxs))
+
+
+def fwer(false_idxs, reject_list):
+    """
+    Given the indexes of false null hypotheses
+    and the indexes of rejected hypotheses of multiple runs
+    Return the FWER
+    """
+    n_hypotheses = len(reject_list)
+    type1_error = 0
+    for r in reject_list:
+        type1_error += type1_occured(false_idxs, r)
+    return type1_error / n_hypotheses
+
+
+def count_rejection(false_idxs, reject_list):
+    """
+    Count the number of rejections for no-type-I-error runs.
+    """
+    counts = []
+    for r in reject_list:
+        n_rejects = len(r)
+        if not type1_occured(false_idxs, r) and n_rejects > 0:
+            counts.append(n_rejects)
+    return counts
+
+
+def ranks(false_idxs, rejected):
+    """
+    Return the maximal utility rank for each run
+    """
+    n_false_null = len(false_idxs)
+    ranks = []
+    for r in rejected:
+        # We only concern runs that no type-I error occurs
+        if not type1_occured(false_idxs, r) and len(r) > 0:
+            optimal_idx = np.where(np.max(r) == false_idxs)[0][0]
+            rank = n_false_null - optimal_idx
+            ranks.append(rank)
+    return np.array(ranks)
+
+
+if __name__ == "__main__":
+    # ===== Analyzing the result =====
+    _fwer = dict()
+    _rank = dict()
+    _count = dict()
+    for exp in exps:
+        _fwer[exp] = dict()
+        _rank[exp] = dict()
+        _count[exp] = dict()
+        for method in methods:
+            print(f"\n==============\n{exp}, {method}")
+            # Read result file
+            f = open_file(sub_dir, exp, method, "r")
+            false_idxs, reject_list = read_result(f)
+
+            # Counting and stuff
+            _fwer[exp][method] = fwer(false_idxs, reject_list)
+            _rank[exp][method] = ranks(false_idxs, reject_list)
+            _count[exp][method] = np.mean(count_rejection(false_idxs, reject_list))
+
+            # Print result
+            print("- FWER:", _fwer[exp][method])
+            print("- RANKS:", np.histogram(_rank[exp][method])[0])
+            print("- AVE-#REJECT:", _count[exp][method])
+
+            # Close file
+            f.close()
+
+    # ===== Summarizing result =====
+    print("\n====== SUMMARY ======")
+    fwer_df = pd.DataFrame.from_dict(_fwer)
+    count_df = pd.DataFrame.from_dict(_count)
+    print(fwer_df)
+    print(count_df)
+
+    # ===== Plotting result =====
+    # We don't plot invalid-spur
+    _methods = [m for m in methods.keys() if m != "Invalid SPUR"]
+    print(_methods)
+    n_methods = len(_methods)
+    n_exps = len(exps)
+
+    linestyles = ["-", "--", ":"]
+    colors = ["b", "g", "r"]
+    fig, axs = plt.subplots(1, 3, figsize=(9, 3.2))
+    axs = axs.flat
+    lines = []
+    labels = []
+    for exd, exp in enumerate(exps):
+        axs[exd].set_title(
+            exp, fontdict={"fontsize": 13},
+        )
+        for med, method in enumerate(_methods):
+            hist = np.histogram(_rank[exp][method], bins=20)[0]
+            ranks = np.arange(len(hist))
+
+            # Plot histogram
+            plot_id = exd
+            line = axs[plot_id].plot(
+                ranks,
+                np.cumsum(hist),
+                label=method,
+                linewidth=3,
+                linestyle=linestyles[med],
+                color=colors[med],
+            )
+            # Settings
+            axs[plot_id].ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
+            axs[plot_id].set_ylim(bottom=0)
+            xticks = np.array([0, 4, 9, 14, 19])
+            xticklabels = xticks + 1
+            axs[plot_id].set_xticks(xticks)
+            axs[plot_id].set_xticklabels(xticklabels)
+            axs[plot_id].set_xlabel("Utility ranking", fontsize=12)
+
+            # Legends
+            if exd == 0:
+                lines.append(line)
+                labels.append(method)
+
+    axs[0].set_ylabel("Number of rejections", fontsize=12)
+    axs[0].set_ylabel("Cumulative counts", fontsize=12)
+    fig.legend(
+        labels=labels, loc="lower center", ncol=3, borderaxespad=0.4, fontsize=12
+    )
+    fig.subplots_adjust(bottom=0.3)
+    plt.savefig(f"output/plot/exp_1.png", bbox_inches="tight")
+    plt.show()
+
+    # ===== Printing result for latex's table format =====
+    # for method in methods:
+    #     s = f"{method} "
+    #     for exp in exps:
+    #         s += "& %.3f " % _fwer[exp][method]
+    #     for exp in exps:
+    #         s += "& %.3f " % _count[exp][method]
+    #     s += "\\\\"
+    #     print(s)
